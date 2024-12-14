22aad2780f6e21586c1f1955c08895311e2d86d5
wind <jamwujustyle@gmail.com>
2024-12-14 17:51:59 +0500

    updated config for docker image

--- index.py
+++ index.py
@@ -36,4 +36,4 @@ app.register_blueprint(order_management.blueprint)
 app.register_blueprint(cart_management.blueprint)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
