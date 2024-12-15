e9137876a428145ebd82d85987b2bb3d50beb1f5
wind <jamwujustyle@gmail.com>
2024-12-15 21:16:07 +0500

    getting complex

--- routes/orders.py
+++ routes/orders.py
@@ -33,7 +33,6 @@ class OrderManagement:
             payment_method = data.get("payment_method")
             payment_status = data.get("payment_status")
             shipping_status = data.get("shipping_status")
-            # total_price = data.get("total_price")
             tracking_number = generate_tracking_number()
             products = data.get("order_items")
             if any(not value for value in [user_id, shipping_address]):
@@ -154,12 +153,11 @@ class OrderManagement:
         @self.blueprint.route("/orders/put/<id>", methods=["PUT"])
         @jwt_required()
         def put(id):
-            claims = get_jwt()
-            if claims.get("role") != "admin":
-                return jsonify({"error": "insufficient permissions"}), 401
+            check_for_admin()
             data = request.get_json()
             if not data:
                 return jsonify({"error": "request body is empty"}), 400
+
             user_id = data.get("user_id")
             status = data.get("status")
             shipping_address = data.get("shipping_address")
@@ -167,82 +165,108 @@ class OrderManagement:
             payment_status = data.get("payment_status")
             shipping_status = data.get("shipping_status")
             tracking_number = data.get("tracking_number")
-            products = data.get("products")
+            products = data.get("order_items")
+
             if any(not value for value in [user_id, status, shipping_address]):
                 return jsonify({"error": "missing required arguments"}), 400
+
             query = f"UPDATE {self.table_name} SET "
             update_fields = []
             params = []
+
             append_update_field(update_fields, params, "user_id", user_id)
             append_update_field(update_fields, params, "status", status)
             append_update_field(
                 update_fields, params, "shipping_address", shipping_address
             )
-
             append_update_field(update_fields, params, "payment_method", payment_method)
             append_update_field(update_fields, params, "payment_status", payment_status)
             append_update_field(
                 update_fields, params, "shipping_status", shipping_status
             )
-            append_update_field(
-                update_fields, params, "tracking_number", tracking_number
-            )
-            query += ", ".join(update_fields) + " WHERE id = %s RETURNING *;"
-            params.append(int(id))
+
             try:
                 with connect() as conn:
-                    cursor = conn.cursor(cursor_factory=DictCursor)
-                    cursor.execute(query, params)
-                    updated = cursor.fetchone()
-
-                    if not updated:
-                        return jsonify({"error": "could not update order"}), 400
-
-                    total_price = 0
-                    for product in products:
-                        product_id = product.get("product_id")
-                        quantity = product.get("quantity")
+                    with conn.cursor(cursor_factory=DictCursor) as cursor:
                         cursor.execute(
-                            "SELECT price FROM products WHERE id = %s;", (product_id,)
+                            "SELECT tracking_number FROM orders WHERE id = %s", (id,)
                         )
-                        product_price_record = cursor.fetchone()
-                        if not product_price_record:
-                            return (
-                                jsonify({"error": f"product with ID {id} not found"}),
-                                404,
-                            )
-                        product_price = product_price_record["price"]
+                        existing_record = cursor.fetchone()
 
-                        total_price += product_price * quantity
-                        order_items_query = f"""
-                        INSERT INTO order_items (order_id, product_id, quantity)
-                        VALUES (%s, %s, %s)
-                        ON CONFLICT (product_id, order_id)
-                        DO UPDATE SET quantity = order_items.quantity + %s;                    
-                        """
+                        if existing_record and existing_record["tracking_number"]:
+                            tracking_number = None
+
+                        if tracking_number:
+                            append_update_field(
+                                update_fields,
+                                params,
+                                "tracking_number",
+                                tracking_number,
+                            )
+
+                        query += (
+                            ", ".join(update_fields) + " WHERE id = %s RETURNING *;"
+                        )
+                        params.append(int(id))
+
+                        cursor.execute(query, params)
+                        updated = cursor.fetchone()
+
+                        if not updated:
+                            return jsonify({"error": "could not update order"}), 400
+
+                        total_price = 0
+                        for product in products:
+                            product_id = product.get("product_id")
+                            quantity = product.get("quantity")
+                            cursor.execute(
+                                "SELECT price FROM products WHERE id = %s;",
+                                (product_id,),
+                            )
+                            product_price_record = cursor.fetchone()
+                            if not product_price_record:
+                                return (
+                                    jsonify(
+                                        {
+                                            "error": f"product with ID {product_id} not found"
+                                        }
+                                    ),
+                                    404,
+                                )
+                            product_price = product_price_record["price"]
+
+                            total_price += product_price * quantity
+                            order_items_query = f"""
+                            INSERT INTO order_items (order_id, product_id, quantity)
+                            VALUES (%s, %s, %s)
+                            ON CONFLICT (product_id, order_id)
+                            DO UPDATE SET quantity = order_items.quantity + %s;                    
+                            """
+                            cursor.execute(
+                                order_items_query,
+                                (
+                                    id,
+                                    product_id,
+                                    quantity,
+                                    quantity,
+                                ),
+                            )
+
                         cursor.execute(
-                            order_items_query,
+                            "UPDATE orders SET total_price = %s WHERE id = %s",
                             (
+                                total_price,
                                 id,
-                                product_id,
-                                quantity,
-                                quantity,
                             ),
                         )
-                    cursor.execute(
-                        "UPDATE orders SET total_price = %s WHERE id = %s",
-                        (
-                            total_price,
-                            id,
-                        ),
-                    )
-                    conn.commit()
-                    return (
-                        jsonify(
-                            {"updated order": "updated", "total price": total_price}
-                        ),
-                        201,
-                    )
+                        conn.commit()
+
+                        return (
+                            jsonify(
+                                {"updated order": "updated", "total price": total_price}
+                            ),
+                            201,
+                        )
             except Exception as ex:
                 return jsonify({"error": str(ex)}), 500
 
